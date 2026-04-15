a0126d3a88ddcb3fe298d5be2b55036be306cdd9
Julien BERNARD <127470227+julienbernard85@users.noreply.github.com>
2026-04-15 10:38:27 +0200

    Refacto Admin

--- app/core/messages.py
+++ app/core/messages.py
@@ -4,6 +4,7 @@ RESERVATION_NOT_FOUND = "Réservation non trouvée"
 ARCADE_NOT_FOUND = "Borne d'arcade non trouvée"
 USER_NOT_FOUND = "Utilisateur non trouvé"
 GAME_NOT_FOUND = "Jeu non trouvé"
+PROMO_CODE_NOT_FOUND = "Code promo non trouvé"
 
 FRIEND_REQUEST_ALREADY_EXISTS = "Demande d'ami déjà en cours"
 ALREADY_FRIENDS = "Vous êtes déjà amis"
